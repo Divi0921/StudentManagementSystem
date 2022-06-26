4f219c02e2979f5a4921be10ec01bcfbb0cd4308
Divya Bharti <divyabharti1464@gmail.com>
2022-06-26 13:14:58 +0530

    Added Css to login page

--- login.php
+++ login.php
@@ -6,18 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="form">    
         <form action="http://localhost/StudentManagementSystem/dashboard.php" method="POST">
-            <input type="text" name="rollNumber" placeholder="Enter Id">
-            <input type="text" name="password" placeholder="Enter Password">
-            <input type="radio" name="role" id="" value="0"> Admin
-            <input type="radio" name="role" id="" value="1"> Staff
-            <input type="radio" name="role" id="" value="2"> Student
-            <input type="submit" value="login">
+            <div><label for=""> Username:</label><input type="text" name="rollNumber" placeholder="Enter Id"></div>
+            <div><label for=""> Password:</label><input type="text" name="password" placeholder="Enter Password"></div>
+            <div><span><input type="radio" name="role" id="" value="0" class="role">Admin</span>
+            <span><input type="radio" name="role" id="" value="1" class="role">Staff</span>
+            <span><input type="radio" name="role" id="" value="2" class="role">Student</span></div>
+            <input type="submit" value="login" class="submit">
         </form>
     </div>
 </body>
